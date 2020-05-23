f5af5d43e5967369cc2ed35124f5ec919341c4fe
psqq <pestov-sa@ya.ru>
2020-05-23 22:19:50 +0300

    Store comments and fix history (redo)

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,11 @@ import { cloneDeep, merge } from 'lodash';
 
 Vue.use(Vuex);
 
+/**
+ * Создание новой заметки
+ * @param {string} title 
+ * @param {{ title: string, done: boolean }[]} todo 
+ */
 function createNote(title = '', todo = []) {
   return {
     id: id(),
@@ -14,6 +19,9 @@ function createNote(title = '', todo = []) {
   };
 }
 
+/**
+ * Начальное состояние хранилища
+ */
 function initialState() {
   return {
     notes: [],
@@ -24,9 +32,10 @@ function initialState() {
 export default new Vuex.Store({
   state: {
     ...initialState(),
+    // Несколько записей для тестирования
     notes: [
       createNote('Create this app', [
-        { title: 'Create home page', done: true },
+        { title: 'Create home page', done: false },
         { title: 'Create note page', done: true },
         { title: 'Create todo component', done: true },
       ]),
@@ -39,40 +48,50 @@ export default new Vuex.Store({
         { title: 'Task 7', done: false },
       ]),
     ],
-    history: [],
-    historyIndex: 0,
+    // Текущая заметка для редактирования
     note: null,
+    // История заметок.
+    history: [],
+    // Текущее положение в истории. Отмена -1. Возврат +1.
+    historyIndex: 0,
   },
   mutations: {
-    emptyState(state) {
-      this.replaceState({ ...initialState(), notes: state.notes, note: state.note });
-    },
+    // Создание новой заметки
     createEmptyNote(state) {
       state.note = createNote();
-      this.commit('addToHistory', state.note);
-    },
-    open(state, payload) {
-      state.note = cloneDeep(state.notes.find(x => x.id === payload.id));
       this.commit('resetHistory');
     },
+    // Открытие заметки с заданным ид
+    open(state, { id }) {
+      state.note = cloneDeep(state.notes.find(x => x.id === id));
+      this.commit('resetHistory');
+    },
+    // Добавление текущего состояния заметки в историю
     addToHistory(state, payload) {
+      state.history = state.history.slice(0, state.historyIndex + 1);
       state.history[++state.historyIndex] = cloneDeep(payload);
     },
+    // Сброс истории
     resetHistory(state) {
       state.history = [cloneDeep(state.note)];
       state.historyIndex = 0;
     },
+    // Удаление заметки
     deleteNote(state, payload) {
       state.notes = state.notes.filter(x => x.id != payload.id);
     },
+    // Установка заголовка для заметки
     setTitle(state, payload) {
       state.note.title = payload.title;
       this.commit('addToHistory', state.note);
     },
+    // Установка списка задач для заметки
     setTodo(state, payload) {
       state.note.todo = cloneDeep(payload.todo);
       this.commit('addToHistory', state.note);
     },
+    // Сохранения текущего состояния заметки в notes или добавления новой заметки,
+    // если ее еще там нет
     saveOrCreateNew(state) {
       const note = state.notes.find(x => x.id === state.note.id);
       if (note) {
@@ -81,6 +100,10 @@ export default new Vuex.Store({
         state.notes.push(state.note);
       }
     },
+    // Отмена всех изменения в текущем окне заметки.
+    // Если заметка есть в notes, то берем ее состояние от туда.
+    // Если нет, то обнуляем до новой заметки.
+    // В конце сбрасываем историю.
     undoAllChanges(state) {
       const note = state.notes.find(x => x.id === state.note.id);
       if (note) {
@@ -90,11 +113,13 @@ export default new Vuex.Store({
       }
       this.commit('resetHistory');
     },
+    // Отмена одного изменения. Переход на один шаг назад в истории изменений
     undo(state) {
       if (state.historyIndex > 0) {
         state.note = cloneDeep(state.history[--state.historyIndex]);
       }
     },
+    // Восстановление отмены. Переход на один шаг вперед в истории изменений
     redo(state) {
       if (state.historyIndex < state.history.length - 1) {
         state.note = cloneDeep(state.history[++state.historyIndex]);
@@ -105,5 +130,6 @@ export default new Vuex.Store({
   },
   modules: {
   },
+  // Плагин для сохранения состояния в localStorage
   plugins: [createPersistedState()],
 });
